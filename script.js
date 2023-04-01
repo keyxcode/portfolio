d4c24af5ac2d912c3e5f653ced53b98746bb92f9
keyxcode <keyxcode@gmail.com>
2023-04-01 17:41:43 +0700

    Implement blinking text

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const header = document.querySelector("body");
+const blinkingText = document.querySelector("#blinking-text");
+const cursor = document.querySelector("#cursor");
+
+let text = "< My Projects />";
+const textArray = [...text];
+
+const blinkingCursor = setInterval(() => {
+  cursor.textContent = cursor.textContent === "|" ? "" : "|";
+}, 500);
+
+window.addEventListener("scroll", () => {
+  console.log("hi");
+
+  setInterval(() => {
+    if (textArray.length === 0) return;
+    blinkingText.textContent += textArray.shift();
+  }, 500);
+});
